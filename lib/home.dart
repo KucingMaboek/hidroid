5ff750546d932393c66aef954e536a9401cae975
PilotJinix <agungsantoso557@gmail.com>
2020-12-04 02:04:18 +0700

    leading false

--- lib/home.dart
+++ lib/home.dart
@@ -20,6 +20,7 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        automaticallyImplyLeading: false,
         title: Text(
           "HIDROID",
           style: TextStyle(
